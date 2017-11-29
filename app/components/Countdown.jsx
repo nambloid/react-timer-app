7619d8ac1e8437e71a935cc6f2d2695b4fa0d98d
Emil Pettersson <emil.pettersson05@gmail.com>
2017-11-29 20:51:56 +0200

    Add Countdown functionality

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -7,18 +7,39 @@ class Countdown extends React.Component {
     constructor (props) {
         super(props);
         this.handleSetCountdown = this.handleSetCountdown.bind(this);
+        this.startTimer = this.startTimer.bind(this);
         this.state = {
-            count: 0
+            count: 0,
+            countdownStatus: 'stopped'
         };
+    }
+    componentDidUpdate (prevProps, prevState) {
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch (this.state.countdownStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+            }
+        }
+    }
+    startTimer () {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count - 1;
+            debugger;
+            this.setState({
+                count: newCount >= 0 ? newCount : 0
+            });
+        }, 1000);
     }
     handleSetCountdown (seconds) {
         this.setState({
-            count: seconds
+            count: seconds,
+            countdownStatus: 'started'
         });
     }
     render () {
         var {count} = this.state;
-        
+
         return (
             <div>
                 <Clock totalSeconds={count}/>
---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import $ from 'jQuery';
+import ReactTestUtils from 'react-dom/test-utils';
+
+import Countdown from 'Countdown';
+
+describe('Countdown', ()=> {
+    it('should exist', ()=> {
+        expect(Countdown).toBeTruthy();
+    });
+
+    describe('handleSetCountdown', ()=> {
+        it('should set state to started and count down', (done)=> {
+            var countdown = ReactTestUtils.renderIntoDocument(<Countdown/>);
+
+            countdown.handleSetCountdown(10);
+
+            expect(countdown.state.count).toBe(10);
+            expect(countdown.state.countdownStatus).toBe('started');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(9);
+                done();
+            }, 1001);
+        });
+
+        it('should set state to started and count down', (done)=> {
+            var countdown = ReactTestUtils.renderIntoDocument(<Countdown/>);
+
+            countdown.handleSetCountdown(1);
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 3001);
+        });
+    });
+});
